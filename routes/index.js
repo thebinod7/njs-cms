2ed6e8864adcee14b8fd8ddc3d96b0ade680a55a
Binod Chaudhary <thebinod7@gmail.com>
2017-03-13 16:14:43 +0545

    Dashboard design

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,16 @@ router.get('/',function (req,res) {
     res.render('index');
 });
 
+router.get('/profile',function (req,res) {
+    res.render('secure/profile');
+});
+
+router.get('/new_post',function (req,res) {
+    res.render('secure/new_post');
+});
+
+router.get('/add_category',function (req,res) {
+    res.render('secure/add_category');
+});
+
 module.exports = router;
