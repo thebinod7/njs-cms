aecd4b545679e69f126a12fca87082faf74679f0
Binod Chaudhary <thebinod7@gmail.com>
2017-04-01 17:19:32 +0545

    Author profile link

--- routes/post.js
+++ routes/post.js
@@ -7,8 +7,7 @@ var multer  = require('multer');
 
 
 var storage = multer.diskStorage({
-   // destination: './public/img/uploads/',
-    destination: 'https://node-cm.herokuapp.com/public/img/uploads/',
+    destination: './public/img/uploads/',
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1])
     }
