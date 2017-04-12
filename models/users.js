ea1fcee60d33be4353772815034972e545eff868
Binod Chaudhary <thebinod7@gmail.com>
2017-04-12 15:43:33 +0545

    Remove footer and minor design changes

---
+++ models/users.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const config = require('../config/database');
+
+var objectId = mongoose.Schema.ObjectId;
+const userSchema = mongoose.Schema({
+    userId : objectId,
+    firstName : {
+        type: String,
+        required : true
+    },
+    lastName : {
+        type : String
+    },
+    contactNumber : {
+        type : String
+    },
+    email : {
+        type : String
+    },
+    password : {
+        type : String
+    },
+    socialUrl : {
+        type : String
+    },
+    role : {
+        type : objectId,
+        ref : 'Role'
+    },
+    isActive : {
+        type : Boolean,
+        default : true
+    },
+    dateAdded : {
+        type : Date,
+        default: Date.now
+    }
+});
+
+const Users = module.exports = mongoose.model('Users',userSchema);
+
